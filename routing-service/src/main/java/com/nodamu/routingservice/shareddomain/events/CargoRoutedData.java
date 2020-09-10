589e63bb503d0e13399f736647266697d8aacecb
nodamu <nickadamu@gmail.com>
2020-09-10 13:11:09 +0000

    Added cargo routed event classes

---
+++ routing-service/src/main/java/com/nodamu/routingservice/shareddomain/events/CargoRoutedData.java
@@ -0,0 +1,24 @@
+package com.nodamu.routingservice.shareddomain.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author profnick
+ * 9/10/20
+ **/
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class CargoRoutedData {
+    private String bookingId;
+
+    public String getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(String bookingId) {
+        this.bookingId = bookingId;
+    }
+}
---
+++ routing-service/src/main/java/com/nodamu/routingservice/shareddomain/events/CargoRoutedEvent.java
@@ -0,0 +1,8 @@
+package com.nodamu.routingservice.shareddomain.events;
+
+/**
+ * @author profnick
+ * 9/10/20
+ **/
+public class CargoRoutedEvent {
+}
