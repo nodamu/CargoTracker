e827cf5dbefc96f11e87aa2e81f0192eb6137967
nodamu <nickadamu@gmail.com>
2020-09-14 22:39:39 +0000

    Clean test code

--- booking-module/src/main/java/com/nodamu/cargotracker/booking/BookingApplication.java
+++ booking-module/src/main/java/com/nodamu/cargotracker/booking/BookingApplication.java
@@ -1,17 +1,6 @@
 package com.nodamu.cargotracker.booking;
 
 import com.nodamu.cargotracker.booking.adapter.out.messagebrokers.CargoEventSource;
-import com.nodamu.cargotracker.booking.application.ports.out.CargoRepository;
-import com.nodamu.cargotracker.booking.application.services.commandservices.BookCargoCommandService;
-import com.nodamu.cargotracker.booking.application.services.commandservices.RouteCargoCommandService;
-import com.nodamu.cargotracker.booking.domain.aggregates.BookingId;
-import com.nodamu.cargotracker.booking.domain.aggregates.Cargo;
-import com.nodamu.cargotracker.booking.domain.commands.BookCargoCommand;
-import com.nodamu.cargotracker.booking.domain.commands.RouteCargoCommand;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -22,22 +11,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
 
-import java.sql.Date;
-import java.util.List;
+
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.nodamu.cargotracker.booking.adapter.out.persistence.model.entities"})  // scan JPA entities
 @EnableJpaRepositories
 @EnableBinding(CargoEventSource.class)
 @EnableDiscoveryClient
-public class BookingApplication  implements CommandLineRunner{
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+public class BookingApplication {
 
-    @Autowired
-    private BookCargoCommandService bookCargoCommandService;
-
-    @Autowired
-    private RouteCargoCommandService routeCargoCommandService;
 
     public static void main(String[] args) {
         SpringApplication.run(BookingApplication.class, args);
@@ -50,17 +32,5 @@ public class BookingApplication  implements CommandLineRunner{
         return new RestTemplate();
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        BookCargoCommand bookCargoCommand = new BookCargoCommand(
-                300,
-                "PUM",
-                "DEL",
-                new Date(2020,9,14)
-        );
-       BookingId id = bookCargoCommandService.bookCargo(bookCargoCommand);
 
-        RouteCargoCommand routeCargoCommand = new RouteCargoCommand(id.getBookingId());
-        routeCargoCommandService.routeCargo(routeCargoCommand);
-}
 }
