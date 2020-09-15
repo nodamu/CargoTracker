21614b745bd2bfc9d8ffc4e98bd665c23837adc2
nodamu <nickadamu@gmail.com>
2020-09-15 23:03:30 +0000

    Added travis ci config

---
+++ docker_push.sh
@@ -0,0 +1,14 @@
+cd booking-module
+
+./gradlew bootBuildImage
+
+cd routing-service
+
+./gradlew bootBuildImage
+
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+docker push nickadamu/booking-service
+docker push nickadamu/routing-service
+
